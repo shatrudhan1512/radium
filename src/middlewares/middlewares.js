adaa28dd21d13694e2b626cb59aa756d1eb0efef
shatrudhan1512 <shatrudhan1512@gmail.com>
2021-11-24 01:53:56 +0530

    updated

--- src/middlewares/middlewares.js
+++ src/middlewares/middlewares.js
@@ -1,16 +1,16 @@
 
 const mid1 = function (req, res, next) {
-    console.log("Hi I am middleware")
+    console.log("Hi I am middleware1")
     next()
 }
 
 const mid2 = function (req, res, next) {
-    console.log("Hi I am middleware")
+    console.log("Hi I am middleware2")
     next()
 }
 
 const mid3 = function (req, res, next) {
-    console.log("Hi I am middleware")
+    console.log("Hi I am middleware3")
     next()
 }
 
