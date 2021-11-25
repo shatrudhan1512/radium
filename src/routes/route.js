64705e62cc24499df6db49d43b6414037affa4bc
Shatrudhan Kumar <93571747+shatrudhan1512@users.noreply.github.com>
2021-11-26 00:36:54 +0530

    Update route.js

--- src/routes/route.js
+++ src/routes/route.js
@@ -7,6 +7,6 @@ const Controller = require('../controllers/Controller')
 
 
 router.get('/whethers', Controller.citiesWhether)
-router.get('/landon/whethers', Controller.londonWhether)
+router.get('/cities/whethers', Controller.londonWhether)
 
 module.exports = router;
